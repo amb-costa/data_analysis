1d4b2d3f41b7a17c79428c76a17666ffcf72df61
amb-costa <ocaranzacosta@gmail.com>
2025-06-18 19:32:26 -0400

    preparing postcleaning code

--- finalproject_1/clean_check.R
+++ finalproject_1/clean_check.R
@@ -1,20 +1,24 @@
-
-# clean_union_all <- read_csv(" ~/data_analysis/finalproject_1/cleaned_table.csv)
+# clean_check : revisión del proceso de limpieza
+# package.install(c(rlang, magrittr, data.table))
+# clean_union_all <- read_csv("~/data_analysis/finalproject_1/cleaned_table.csv)
+# estrategia <- c(existencia, unicidad, viaje válido)
 library(rlang)
-install.packages("dplyr")
+library(magrittr)
+library(data.table)
 
-cleaned_table <- setDT(clean_union_all)
-testing <- sapply(cleaned_table, anyNA)
+pre_tabla <- as.data.table(clean_union_all)
 
+#existencia <- ningún registro es null
+# esto confirma que no hay registros NULL 
+# testing <- lapply(pre_tabla, anyNA)
+
+# unicidad <- la información no está duplicada
+# %>% los id de viaje y de estación son únicos
 # verificando que no hay id's de viaje duplicados
 # si los hay, este codigo entrega una lista = 121 duplicados
-tested <- table(cleaned_table$ride_id)
-notrepeating <- data.table(table(cleaned_table$ride_id))
-repeating <- notrepeating[notrepeating$Freq > 1,]
-repeated <- cleaned_table[cleaned_table$ride_id %in% notrepeating$Var1[notrepeating$Freq > 1],]
+# id_viajes <- data.frame(table(pre_tabla$ride_id))
+# viajes_na <- lapply(id_viajes, anyNA)
+# tabla_repetidos <- pre_tabla[pre_tabla$ride_id %in% id_viajes$Var1[id_viajes$Freq > 1],]
 
-repetidos  <- cleaned_table %<%
-  distinct()
+tabla_limpia <- unique(pre_tabla, by="ride_id")
 
-ans <- cleaned_table[cleaned_table$ride_id[ride_id$Freq > 1, ]]
-head(ans)
